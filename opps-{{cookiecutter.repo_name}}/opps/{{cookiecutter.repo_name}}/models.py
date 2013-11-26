997f98c75532a8ae80752fab620863319fc3e240
Bruno Rocha <rochacbruno@gmail.com>
2013-11-26 14:32:23 -0200

    Update models.py

---
+++ opps-{{cookiecutter.repo_name}}/opps/{{cookiecutter.repo_name}}/models.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from django.db import models
+from opps.containers.models import Container
+
+
+class {{cookiecutter.repo_name.title()}}(Container):
+    """ Container derivative for from opps.containers.models import Container """
