2e8a2bfe91a43adb5d18f8bfb7f4249aaa13e4c8
Igor P. Leroy <ip.leroy@gmail.com>
2014-07-02 21:06:48 -0300

    Fix admin.py skeleton

--- opps-{{cookiecutter.repo_name}}/opps/{{cookiecutter.repo_name}}/admin.py
+++ opps-{{cookiecutter.repo_name}}/opps/{{cookiecutter.repo_name}}/admin.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 from opps.core.widgets import OppsEditor
-from opps.core.admin import PublishableAdmin
-from opps.core.admin import apply_opps_rules
+from opps.core.admin import PublishableAdmin, opps_apps_rules
 
 from .models import {{cookiecutter.repo_name.title()}}
-
 
 
 @apply_opps_rules('{{cookiecutter.repo_name}}')
 class {{cookiecutter.repo_name.title()}}Admin(PublishableAdmin):
     """ {{cookiecutter.repo_name}} Admin """
+    class Meta:
+        model = {{cookiecutter.repo_name.title()}}
 
 
 admin.site.register({{cookiecutter.repo_name.title()}}, {{cookiecutter.repo_name.title()}}Admin)
